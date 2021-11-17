3c1d2d8436381d97cabd14fbbcebf2d4b23bd976
arora-ansh <ansh19022@iiitd.ac.in>
2021-11-18 02:39:07 +0530

    somethin

---
+++ MC/MC_ES.py
@@ -0,0 +1,8 @@
+# Monte Carlo Control using Exploring Starts 
+
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = gym.make('Blackjack-v1')
+
--- MC/MC_OnControl.py
+++ MC/MC_OnControl.py
@@ -1,3 +1,8 @@
 # In Control of MC, we use MC Prediction to figure out the State Values (Policy Evaluation), and then use these to figure out the Policy (Policy Improvement).
 # This is an extension to The Generalized Policy Iteration Algorithm, where here in this case we just use MC to get the State Values.
 
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = gym.make('Blackjack-v1')
