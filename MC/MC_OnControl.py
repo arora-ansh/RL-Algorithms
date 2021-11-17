5a063568a1d63a94940cf7b9d01c6892ab2d2fcb
arora-ansh <ansh19022@iiitd.ac.in>
2021-11-17 22:52:06 +0530

    MC

---
+++ MC/MC_OnControl.py
@@ -0,0 +1,3 @@
+# In Control of MC, we use MC Prediction to figure out the State Values (Policy Evaluation), and then use these to figure out the Policy (Policy Improvement).
+# This is an extension to The Generalized Policy Iteration Algorithm, where here in this case we just use MC to get the State Values.
+
